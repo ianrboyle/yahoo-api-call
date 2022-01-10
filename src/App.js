78678cb29aa0a2b6545049debbc4f407d6bafea2
Ian Boyle <irboyle21@gmail.com>
2022-01-10 10:09:15 -0500

    adds callAPI function to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import axios, { Axios } from "axios";
 function App() {
-  return <div className="App"></div>;
+  const callApi = () => {
+    axios.get("https://v2.jokeapi.dev/joke/Any").then((response) => {
+      console.log(response);
+    });
+  };
+  return (
+    <div className="App">
+      <button onClick={callApi}>Get Joke</button>
+    </div>
+  );
 }
 
 export default App;
